e6477190e7d7c29c09c60d45681b56bde088eead
Yash Saxena <30472599+yashsaxena300@users.noreply.github.com>
2026-01-04 02:21:11 -0500

    pwa builder packaging

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'focus-todo-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
